b7211ecd13c8eb6989a349c0fa7090c082e1bfd7
Felixson Yusuf Tosin <felixsonyusuftosin@gmail.com>
2020-01-21 13:25:52 +0100

    add quicksort doc

--- qsort.js
+++ qsort.js
@@ -1,9 +1,9 @@
 /**
  * @file 
+ * QuickSort
 */
 
 const getPivot = (arr) => Math.round( (arr.length -1)/2 )
-
 const qsort = ( arr ) => { 
   if ( arr.length < 2) return arr
   const pivot = getPivot(arr)
@@ -14,23 +14,8 @@ const qsort = ( arr ) => {
     newArr[i] > arr[pivot] ? greater.push(newArr[i]) : less.push(newArr[i])
   }
   return [...qsort(less), arr[pivot], ...qsort(greater) ] 
-
 }
 
-  const firstCase = '741784427485714875845728754957214752857454857484842874285885708542754074752'.split('')
-  const secondCase = [100,99,88,34,12,0,45,12,24,25,4,0, 75, 999, 88]
-  const thirdCase = [1]
-  const fourthCase = [67, 23, 33, 12, 1000, -12, -12, -23.3333, 0.345, 0.987]
-  const fifthCase = [ ]
-
-  console.log( 
-    `
-
-   FourthCase : : =>   ${qsort(fourthCase)} \n
-
-
-     `
-  )
 
     
   
